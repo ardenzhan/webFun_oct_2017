3ef4a2d539f6278bd57194fa40d616ce8226a523
Arden <arden@protonmail.ch>
2017-10-23 13:56:49 -0500

    committing declare js assignment

---
+++ arden_zhan/Javascript/Foundation Concepts/declare.js
@@ -0,0 +1,32 @@
+// Arden Zhan
+// I Do Declare!
+/* Declare variables of the following types:
+4 Numbers
+3 Strings
+2 Booleans
+1 Undefined Variable
+Give the variables descriptive names (ex: string1 and string2)
+Print each one to the console with a description (ex: console.log("The First String:", string1);)
+Try to be creative! (don't use 1, 2, 3, and 4 for your numbers -- test out negatives or decimals!) */
+
+var num1 = 1;
+var num2 = 2.3;
+var num3 = -3;
+var num4 = 4;
+var string1 = "IDK";
+var string2 = "what";
+var string3 = "to do";
+var bool1 = true;
+var bool2 = false;
+var something = null;
+
+console.log("The first number is: ", num1);
+console.log("The second number is: ", num2);
+console.log("The third number is: ", num3);
+console.log("The fourth number is: ", num4);
+console.log("The First String:", string1);
+console.log("The Second String:", string2);
+console.log("The Third String:", string3);
+console.log(bool1, "true bool");
+console.log(bool2, "false bool");
+console.log("printing something", something);
